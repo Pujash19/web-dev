c799f26e0c4bb5ce4eb523239df86541a6a6c540
Kumari Puja Sharma <140588494+Pujash19@users.noreply.github.com>
2024-06-21 17:20:43 +0530

    Delete Posting-App-PHP-main directory

--- Posting-App-PHP-main/app/config/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
- //connect to database
-    $conn = mysqli_connect('localhost', 'your_username', 'your_password', 'postsdb');
-    //check connection
-    if(!$conn){
-        echo 'Connection error : ' . mysqli_connect_error();
-    }
-?>
--- Posting-App-PHP-main/app/templates/footer.php
+++
@@ -1,4 +0,0 @@
-    <footer class="section">
-        <div class="center grey-text">Copyright &copy 2022 Medox</div>
-    </footer>
-</body>
